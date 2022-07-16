1bad1a2d0d18b4b54857b1725fe6c96ecfc8a28a
Amirhossein Alibabaei <56980445+FakeSudo@users.noreply.github.com>
2022-07-16 20:31:40 +0430

    Update init.lua

--- plugins/init.lua
+++ plugins/init.lua
@@ -24,19 +24,19 @@ return {
       config = function ()
          vim.cmd [[
             let g:silicon = {
-               \   'theme':          'OneHalfDark',
-               \   'font':'Fira Code,Fira Code Medium',
-               \   'background':         '#282c34',
-               \   'shadow-color':       '#282c34',
-               \   'line-pad':                   2,
-               \   'pad-horiz':                  0,
-               \   'pad-vert':                   0,
-               \   'shadow-blur-radius':         0,
-               \   'shadow-offset-x':            0,
-               \   'shadow-offset-y':            0,
-               \   'line-number':           v:true,
-               \   'round-corner':          v:true,
-               \   'window-controls':      v:false,
+               \   'theme':               'OneHalfDark',
+               \   'font': 'Fira Code,Fira Code Medium',
+               \   'background':              '#282c34',
+               \   'shadow-color':            '#282c34',
+               \   'line-pad':                        2,
+               \   'pad-horiz':                       0,
+               \   'pad-vert':                        0,
+               \   'shadow-blur-radius':              0,
+               \   'shadow-offset-x':                 0,
+               \   'shadow-offset-y':                 0,
+               \   'line-number':                v:true,
+               \   'round-corner':               v:true,
+               \   'window-controls':           v:false,
                \ }
             let g:silicon['output'] = '/tmp/silicon-{time:%Y-%m-%d-%H%M%S}.png'
          ]]
